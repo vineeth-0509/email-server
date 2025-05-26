b4caae2d45aca6d1f657ea104128d1ffdbc4c72b
Vineeth-0509 <vineeththungani@gmail.com>
2025-05-26 14:05:54 +0530

    modified db.ts

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,14 +1,16 @@
+import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 
 dotenv.config();
-import { PrismaClient } from "@prisma/client";
 
 const createPrismaClient = () => new PrismaClient();
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
+  prisma: PrismaClient | undefined;
 };
 
 export const db = globalForPrisma.prisma ?? createPrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
